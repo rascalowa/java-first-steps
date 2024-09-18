5e4b42ea60824eef13ce432260407179948f5e8b
Monika Sikora <monika.sikora@connectpoint.pl>
2024-09-18 12:46:12 +0200

    Project creation

---
+++ java-amigos/src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println(number++);
+        System.out.println(number);
+
+        // Reference Types / Objects
+        int age = 21; // primitive
+        Point pointA = new Point(10,10); // object
+    }
+}
