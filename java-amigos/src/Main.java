f7660801296cbf0d651a9c3dfaaf0cafac3a8c0b
Monika Sikora <monika.sikora@connectpoint.pl>
2024-09-18 14:01:10 +0200

    Compilation fixed

--- java-amigos/src/Main.java
+++ java-amigos/src/Main.java
@@ -7,7 +7,14 @@ public class Main {
         System.out.println(number);
 
         // Reference Types / Objects
-        int age = 21; // primitive
-        Point pointA = new Point(10,10); // object
+//        int age = 21; // primitive
+//        Point pointA = new Point(10,10); // object
+
+//        int[] numbers = new int[3];
+//        numbers[0] = 1;
+//        numbers[1] = 22;
+//        numbers[2] = 6;
+//
+//        System.out.println(numbers);
     }
 }
